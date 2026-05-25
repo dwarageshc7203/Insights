2840f7226aad8b9878feb992e7e2b2593d619570
Dwaragesh C <dwarageshc7203@gmail.com>
2026-05-25 20:56:15 +0530

    Added backend files

---
+++ backend/src/test/java/com/dwaragesh/insights/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dwaragesh.insights;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
